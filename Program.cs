24f72616de409201b268aca51d3507df9daa59b5
Nikola Krstevski <nikolakrstevskids@gmail.com>
2022-12-01 17:37:43 +0100

    fatto

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using csharp_oop_shop_2;
 WaterBottle SanPelegrino = new WaterBottle("San Pellegrino", "Aqua dal bergamo", 2, 5, 1.5, 4.5, "Pellegrino Therme");
 Snack bare = new Snack("Bare", "Frutta secca", 1, 5,4, "Mella");
 Applieance Ariston= new Applieance("Ariston", " cestello spazioso e ampio da 10 kg, Centrifuga a 1400 giri", 700, 22, 7600, "bianco", "D");
-
+CannedFood barilla = new CannedFood("Pomodori Confit", "Pomodorii alla confit di barilla", 5, 22, 250, 400);
 SanPelegrino.PrintProduct();
 bare.PrintProduct();
 Ariston.PrintProduct();
@@ -18,3 +18,4 @@ SanPelegrino.refill(2);
 SanPelegrino.PrintProduct();
 SanPelegrino.litterstodrink(2);
 SanPelegrino.PrintProduct();
+barilla.PrintProduct();
